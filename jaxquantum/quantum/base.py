4e69b7e9a25adfed38b5571b2861d53f893c9ded
Phionx <shanjha@mit.edu>
2024-02-26 22:41:57 -0500

    fixed dag , batch_dag

--- jaxquantum/quantum/base.py
+++ jaxquantum/quantum/base.py
@@ -78,7 +78,17 @@ def dag(op: jnp.ndarray) -> jnp.ndarray:
     Returns:
         conjugate transpose of op
     """
-    # return jnp.conj(op).T
+    return jnp.conj(op).T
+
+def batch_dag(op: jnp.ndarray) -> jnp.ndarray:
+    """Conjugate transpose.
+
+    Args:
+        op: operator
+
+    Returns:
+        conjugate of op, and transposes last two axes
+    """
     return jnp.moveaxis(jnp.conj(op), -1, -2) # transposes last two axes, good for batching
 
 
