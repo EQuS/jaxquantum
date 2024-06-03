ff78fe6a554ba96cd33f1d10fa7f8b67f31af492
Shantanu Jha <shanjha@mit.edu>
2024-06-03 15:00:14 -0400

    made solver options flexible

--- jaxquantum/core/solvers.py
+++ jaxquantum/core/solvers.py
@@ -3,19 +3,12 @@
 from functools import partial
 from typing import Callable, List, Optional
 
-from diffrax import diffeqsolve, Dopri5, ODETerm, SaveAt, PIDController
+from diffrax import diffeqsolve, ODETerm, SaveAt, PIDController
+import diffrax
+
 from jax import jit, vmap, Array
 import jax.numpy as jnp
 
-from jaxquantum.utils.utils import (
-    real_to_complex_iso_matrix,
-    real_to_complex_iso_vector,
-    complex_to_real_iso_matrix,
-    complex_to_real_iso_vector,
-    imag_times_iso_vector,
-    imag_times_iso_matrix,
-    conj_transpose_iso_matrix,
-)
 
 from jaxquantum.core.qarray import Qarray
 from jaxquantum.core.conversions import jnps2jqts, jqts2jnps
@@ -70,9 +63,45 @@ def spre(op: Array) -> Callable[[Array], Array]:
         2 * op @ rho @ op_dag - rho @ op_dag @ op - op_dag @ op @ rho
     )
 
+
+def solve(ρ0, f, t_list, args, solver_options):
+    """ Gets teh desired solver from diffrax.
+
+    Args:
+        solver_options: dictionary with solver options
+
+    Returns:
+        solution 
+    """
+
+    # f and ts
+    term = ODETerm(f)
+    saveat = SaveAt(ts=t_list)
+
+    # solver 
+    solver_name = solver_options.get("solver", "Tsit5")
+    solver = getattr(diffrax, solver_name)()
+    stepsize_controller = PIDController(rtol=1e-6, atol=1e-6)
+
+    # solve!
+    sol = diffeqsolve(
+        term,
+        solver,
+        t0=t_list[0],
+        t1=t_list[-1],
+        dt0=t_list[1] - t_list[0],
+        y0=ρ0,
+        saveat=saveat,
+        stepsize_controller=stepsize_controller,
+        args=args,
+        max_steps=solver_options.get("max_steps", 100_000),
+    )
+
+    return sol
+
 @partial(
     jit,
-    static_argnums=(4,),
+    static_argnums=(4,5),
 )
 def mesolve(
     ρ0: Qarray,
@@ -80,6 +109,7 @@ def mesolve(
     c_ops: Optional[List[Qarray]] = None,
     H0: Optional[Qarray] = None,
     Ht: Optional[Callable[[float], Qarray]] = None,
+    solver_options: Optional[dict] = None,
 ):
     """Quantum Master Equation solver.
 
@@ -89,6 +119,7 @@ def mesolve(
         c_ops: list of collapse operators
         H0: time independent Hamiltonian. If H0 is not None, it will override Ht.
         Ht: time dependent Hamiltonian function.
+        solver_options: dictionary with solver options
 
     Returns:
         list of states
@@ -98,6 +129,7 @@ def mesolve(
     c_ops = c_ops or []
     c_ops = jnp.asarray([c_op.data for c_op in c_ops]) + 0.0j
     H0 = jnp.asarray(H0.data) + 0.0j if H0 is not None else None
+    solver_options = solver_options or {}
 
     def f(
         t: float,
@@ -120,35 +152,21 @@ def mesolve(
 
         return rho_dot
 
-    term = ODETerm(f)
-    solver = Dopri5()
-    saveat = SaveAt(ts=t_list)
-    stepsize_controller = PIDController(rtol=1e-6, atol=1e-8)
-
-    sol = diffeqsolve(
-        term,
-        solver,
-        t0=t_list[0],
-        t1=t_list[-1],
-        dt0=t_list[1] - t_list[0],
-        y0=ρ0,
-        saveat=saveat,
-        stepsize_controller=stepsize_controller,
-        args=[H0, c_ops],
-        max_steps=16**5,
-    )
+    
+    sol = solve(ρ0, f, t_list, [H0, c_ops], solver_options)
 
     return jnps2jqts(sol.ys, dims=dims)
 
 @partial(
     jit,
-    static_argnums=(3,),
+    static_argnums=(3,4),
 )
 def sesolve(
     ψ: Qarray,
     t_list: Array,
     H0: Optional[Qarray] = None,
     Ht: Optional[Callable[[float], Qarray]] = None,
+    solver_options: Optional[dict] = None,
 ):
     """Schrödinger Equation solver.
 
@@ -157,6 +175,7 @@ def sesolve(
         t_list: time list
         H0: time independent Hamiltonian. If H0 is not None, it will override Ht.
         Ht: time dependent Hamiltonian function.
+        solver_options: dictionary with solver options
 
     Returns:
         list of states
@@ -166,6 +185,8 @@ def sesolve(
 
     ψ = jnp.asarray(ψ.data) + 0.0j
     H0 = jnp.asarray(H0.data) + 0.0j if H0 is not None else None
+    solver_options = solver_options or {}
+
 
     def f(
         t: float,
@@ -184,186 +205,10 @@ def sesolve(
 
         return ψₜ_dot
 
-    term = ODETerm(f)
-    solver = Dopri5()
-    saveat = SaveAt(ts=t_list)
-    stepsize_controller = PIDController(rtol=1e-6, atol=1e-6)
 
-    sol = diffeqsolve(
-        term,
-        solver,
-        t0=t_list[0],
-        t1=t_list[-1],
-        dt0=t_list[1] - t_list[0],
-        y0=ψ,
-        saveat=saveat,
-        stepsize_controller=stepsize_controller,
-        args=[H0],
-    )
+    sol = solve(ψ, f, t_list, [H0], solver_options)
+
 
     return jnps2jqts(sol.ys, dims=dims)
 
 # ----
-
-
-
-# ----
-
-def spre_iso(op: Array) -> Callable[[Array], Array]:
-    """Superoperator generator.
-
-    Args:
-        op: operator to be turned into a superoperator
-
-    Returns:
-        superoperator function
-    """
-    op_dag = conj_transpose_iso_matrix(op)
-    return lambda rho: 0.5 * (
-        2 * op @ rho @ op_dag - rho @ op_dag @ op - op_dag @ op @ rho
-    )
-
-@partial(
-    jit,
-    static_argnums=(4,),
-)
-def mesolve_iso(
-    ρ0: Qarray,
-    t_list: Array,
-    c_ops: Optional[List[Qarray]] = None,
-    H0: Optional[Qarray] = None,
-    Ht: Optional[Callable[[float], Qarray]] = None,
-):
-    """Quantum Master Equation solver.
-
-    Args:
-        ρ0: initial state, must be a density matrix. For statevector evolution, please use sesolve.
-        t_list: time list
-        c_ops: list of collapse operators
-        H0: time independent Hamiltonian. If H0 is not None, it will override Ht.
-        Ht: time dependent Hamiltonian function.
-
-    Returns:
-        list of states
-    """
-    
-    dims = ρ0.dims
-
-    ρ0 = jnp.asarray(ρ0.data) + 0.0j
-    c_ops = c_ops or []
-    c_ops = jnp.asarray([c_op.data for c_op in c_ops]) + 0.0j
-    H0 = jnp.asarray(H0.data) + 0.0j if H0 is not None else None
-
-    ρ0 = complex_to_real_iso_matrix(jnp.asarray(ρ0) + 0.0j)
-    c_ops = vmap(complex_to_real_iso_matrix)(jnp.asarray(c_ops) + 0.0j)
-    H0 = None if H0 is None else complex_to_real_iso_matrix(jnp.asarray(H0) + 0.0j)
-
-    def f(
-        t: float,
-        rho: Array,
-        args: Array,
-    ):
-        H0_val = args[0]
-        c_ops_val = args[1]
-
-        if H0_val is not None:
-            H = H0_val  # use H0 if given
-        else:
-            H = Ht(t).data  # type: ignore
-            H = complex_to_real_iso_matrix(H + 0.0j)
-
-        rho_dot = -1 * imag_times_iso_matrix(H @ rho - rho @ H)
-
-        for op in c_ops_val:
-            rho_dot += spre(op)(rho)
-
-        return rho_dot
-
-    term = ODETerm(f)
-    solver = Dopri5()
-    saveat = SaveAt(ts=t_list)
-    stepsize_controller = PIDController(rtol=1e-7, atol=1e-7)
-
-    sol = diffeqsolve(
-        term,
-        solver,
-        t0=t_list[0],
-        t1=t_list[-1],
-        dt0=t_list[1] - t_list[0],
-        y0=ρ0,
-        saveat=saveat,
-        stepsize_controller=stepsize_controller,
-        args=[H0, c_ops],
-        max_steps=16**5,
-    )
-
-    return jnps2jqts(vmap(real_to_complex_iso_matrix)(sol.ys), dims=dims)
-
-@partial(
-    jit,
-    static_argnums=(3,),
-)
-def sesolve_iso(
-    ψ: Qarray,
-    t_list: Array,
-    H0: Optional[Qarray] = None,
-    Ht: Optional[Callable[[float], Qarray]] = None,
-):
-    """Schrödinger Equation solver.
-
-    Args:
-        ψ: initial statevector
-        t_list: time list
-        H0: time independent Hamiltonian. If H0 is not None, it will override Ht.
-        Ht: time dependent Hamiltonian function.
-
-    Returns:
-        list of states
-    """
-    dims = ψ.dims
-
-    ψ = jnp.asarray(ψ.data) + 0.0j
-    H0 = jnp.asarray(H0.data) + 0.0j if H0 is not None else None
-    
-    ψ = complex_to_real_iso_vector(jnp.asarray(ψ) + 0.0j)
-    H0 = None if H0 is None else complex_to_real_iso_matrix(jnp.asarray(H0) + 0.0j)
-
-    def f(
-        t: float,
-        ψₜ: Array,
-        args: Array,
-    ):
-        H0_val = args[0]
-
-        if H0_val is not None:
-            H = H0_val  # use H0 if given
-        else:
-            H = Ht(t).data  # type: ignore
-            H = complex_to_real_iso_matrix(H)
-        # print("H", H.shape)
-        # print("psit", ψₜ.shape)
-        ψₜ_dot = -1 * imag_times_iso_vector(H @ ψₜ)
-
-        return ψₜ_dot
-
-    term = ODETerm(f)
-    solver = Dopri5()
-    saveat = SaveAt(ts=t_list)
-    stepsize_controller = PIDController(rtol=1e-7, atol=1e-7)
-
-    sol = diffeqsolve(
-        term,
-        solver,
-        t0=t_list[0],
-        t1=t_list[-1],
-        dt0=t_list[1] - t_list[0],
-        y0=ψ,
-        saveat=saveat,
-        stepsize_controller=stepsize_controller,
-        args=[H0],
-    )
-
-    return jnps2jqts(vmap(real_to_complex_iso_vector)(sol.ys), dims=dims)
-
-# ----
-
