2c0526231bf970bd02e8145a6d4b45bf148d4da8
Phionx <shanjha@mit.edu>
2025-09-26 14:57:51 -0400

    many notebooks related to bosonic

---
+++ experiments/bosonic/misc/cqobjevo_compiled_coeff_7283933320590.pyx
@@ -0,0 +1,31 @@
+#!python
+#cython: language_level=3
+# This file is generated automatically by QuTiP.
+
+import numpy as np
+cimport numpy as np
+import scipy.special as spe
+cimport cython
+np.import_array()
+cdef extern from "numpy/arrayobject.h" nogil:
+    void PyDataMem_NEW_ZEROED(size_t size, size_t elsize)
+    void PyArray_ENABLEFLAGS(np.ndarray arr, int flags)
+from qutip.cy.spmatfuncs cimport spmvpy
+from qutip.cy.inter cimport _spline_complex_t_second, _spline_complex_cte_second
+from qutip.cy.inter cimport _spline_float_t_second, _spline_float_cte_second
+from qutip.cy.inter cimport _step_float_cte, _step_complex_cte
+from qutip.cy.inter cimport _step_float_t, _step_complex_t
+from qutip.cy.interpolate cimport (interp, zinterp)
+from qutip.cy.cqobjevo_factor cimport StrCoeff
+from qutip.cy.cqobjevo cimport CQobjEvo
+from qutip.cy.math cimport erf, zerf
+from qutip.qobj import Qobj
+cdef double pi = 3.14159265358979323
+
+include '/opt/miniconda3/envs/bosonic-env/lib/python3.8/site-packages/qutip/cy/complex_math.pxi'
+
+cdef class CompiledStrCoeff(StrCoeff):
+
+    cdef void _call_core(self, double t, complex * coeff):
+
+        coeff[0] = cos(t)
